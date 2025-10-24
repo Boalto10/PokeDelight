cceeee53c016ba71c6d44c818984679c42ca1b47
Boalto10 <gustavoehelma@gmail.com>
2025-10-24 10:43:03 -0400

    Primeiro commit para teste e para salvar alguma coisa

---
+++ src/main/java/com/mycompany/pokedelight/PokeDelight.java
@@ -0,0 +1,7 @@
+package com.mycompany.pokedelight;
+public class PokeDelight {
+
+    public static void main(String[] args) {
+        System.out.println("Esse é o PokeDelight, Bem vindo!!!");
+    }
+}
